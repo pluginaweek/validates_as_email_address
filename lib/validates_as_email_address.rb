0581a26d64e8e6e26db2a7f4d9bee839ed396390
Aaron Pfeifer <aaron.pfeifer@gmail.com>
2009-01-11 18:37:37 -0500

    Reformat docs

--- lib/validates_as_email_address.rb
+++ lib/validates_as_email_address.rb
@@ -14,25 +14,35 @@ module ValidatesAsEmailAddress
   # limits, i.e. between 3 and 320 characters in length.
   # 
   # Configuration options for length:
-  # * +minimum+ - The minimum size of the attribute
-  # * +maximum+ - The maximum size of the attribute
-  # * +is+ - The exact size of the attribute
-  # * +within+ - A range specifying the minimum and maximum size of the attribute
-  # * +in+ - A synonym(or alias) for :within
-  # * +too_long+ - The error message if the attribute goes over the maximum (default is: "is too long (maximum is %d characters)")
-  # * +too_short+ - The error message if the attribute goes under the minimum (default is: "is too short (minimum is %d characters)")
-  # * +wrong_length+ - The error message if using the :is method and the attribute is the wrong size (default is: "is the wrong length (should be %d characters)")
+  # * <tt>:minimum</tt> - The minimum size of the attribute
+  # * <tt>:maximum</tt> - The maximum size of the attribute
+  # * <tt>:is</tt> - The exact size of the attribute
+  # * <tt>:within</tt> - A range specifying the minimum and maximum size of the
+  #   attribute
+  # * <tt>:in</tt> - A synonym (or alias) for <tt>:within</tt>
+  # * <tt>:too_long</tt> - The error message if the attribute goes over the
+  #   maximum (default is: "is too long (maximum is %d characters)")
+  # * <tt>:too_short</tt> - The error message if the attribute goes under the
+  #   minimum (default is: "is too short (minimum is %d characters)")
+  # * <tt>:wrong_length</tt> - The error message if using the <tt>:is</tt>
+  #   method and the attribute is the wrong size (default is:
+  #   "is the wrong length (should be %d characters)")
   # 
   # Configuration options for format:
-  # * +wrong_format+ - A custom error message (default is: "is an invalid email address")
+  # * <tt>:wrong_format</tt> - A custom error message (default is:
+  #   "is an invalid email address")
   # 
   # Miscellaneous configuration options:
-  # * +allow_nil+ - Attribute may be nil; skip validation.
-  # * +on+ - Specifies when this validation is active (default is :save, other options :create, :update)
-  # * +if+ - Specifies a method, proc or string to call to determine if the validation should
-  # occur (e.g. :if => :allow_validation, or :if => lambda { |user| user.signup_step > 2 }).  The
-  # method, proc or string should return or evaluate to a true or false value.
-  # * +strict+ - Specifies if the domain part of the email should be compliant to RFC 1035 (default is true). If set to false domains such as '-online.com', '[127.0.0.1]' become valid.
+  # * <tt>:allow_nil</tt> - Attribute may be nil; skip validation.
+  # * <tt>:on</tt> - Specifies when this validation is active (default is
+  #   :save, other options :create, :update)
+  # * <tt>:if</tt> - Specifies a method, proc or string to call to determine if
+  #   the validation should occur (e.g. :if => :allow_validation, or
+  #   :if => lambda { |user| user.signup_step > 2 }).  The method, proc or
+  #   string should return or evaluate to a true or false value.
+  # * <tt>:strict</tt> - Specifies if the domain part of the email should be
+  #   compliant to RFC 1035 (default is true). If set to false domains such as
+  #   '-online.com', '[127.0.0.1]' become valid.
   def validates_as_email_address(*attr_names)
     configuration = attr_names.last.is_a?(Hash) ? attr_names.pop : {}
     configuration.reverse_merge!(
